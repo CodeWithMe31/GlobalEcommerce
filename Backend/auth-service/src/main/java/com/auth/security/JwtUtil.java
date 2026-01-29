3635bc060728e31a0e8979a65ac505d3219a7c28
CodeWithMe31 <pvsaikishore22@gmail.com>
2026-01-29 18:06:15 +0530

    Update JwtUtil.java

--- Backend/auth-service/src/main/java/com/auth/security/JwtUtil.java
+++ Backend/auth-service/src/main/java/com/auth/security/JwtUtil.java
@@ -3,20 +3,24 @@ package com.auth.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
-  private static final String SECRET = "CHANGE_THIS_SECRET";
+  @Value(${jwt.secret.key})
+  private String secret;
   private static final long EXPIRY = 1000 * 60 * 60 * 24;
 
-  public static String generateToken(String email, String role) {
+  public String generateToken(String email, String role) {
     return Jwts.builder()
       .setSubject(email)
       .claim("role", role)
       .setIssuedAt(new Date())
       .setExpiration(new Date(System.currentTimeMillis() + EXPIRY))
-      .signWith(SignatureAlgorithm.HS256, SECRET)
+      .signWith(SignatureAlgorithm.HS256, secret)
       .compact();
   }
 }
